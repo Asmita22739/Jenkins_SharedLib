21680447853f97d540e29208f20d6c9b773cdd76
Asmita22739 <sonawaneasmita8@gmail.com>
2025-09-13 22:17:20 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,8 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+def call(sonarTool, projectKey, projectName){
+    sh "${tool(sonarTool)}/bin/sonar-scanner \
+        -Dsonar.projectKey=${projectKey} \
+        -Dsonar.projectName=${projectName} \
+        -Dsonar.sources=backend,frontend \
+        -Dsonar.host.url=http://3.91.23.174:9000
+        -Dsonar.login=squ_dc4b7ae47951d1d4a52e9f63b8d6e22a8b029339"
 }
