ad1c190d214ac9e3fd1159bf473d54f1648b0550
Asmita22739 <sonawaneasmita8@gmail.com>
2025-09-13 21:41:10 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,5 @@
 def call(){
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheck additionalArguments: '--disableNodeAudit'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
